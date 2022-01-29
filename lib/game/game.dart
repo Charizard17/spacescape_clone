71f1eab1ce4fadcaf75655e8b437e79410674a43
Esad Dursun <3723esad@gmail.com>
2022-01-29 20:18:26 +0100

    tilesheet 2 added. game file edited. create player using spritesheet and add it to game

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,5 +1,27 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
 
 class SpacescapeGame extends FlameGame {
-  
+  @override
+  Future<void>? onLoad() async {
+    await images.load('simpleSpace_tilesheet_2.png');
+
+    final spriteSheet = SpriteSheet.fromColumnsAndRows(
+      image: images.fromCache('simpleSpace_tilesheet_2.png'),
+      columns: 8,
+      rows: 6,
+    );
+
+    SpriteComponent player = SpriteComponent(
+      sprite: spriteSheet.getSpriteById(19),
+      size: Vector2(80, 80),
+      position: canvasSize / 2,
+      anchor: Anchor.center,
+    );
+
+    add(player);
+
+    return super.onLoad();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 
 import './game/game.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
   final SpacescapeGame game = SpacescapeGame();
 
   runApp(GameWidget(game: game));
