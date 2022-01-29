95684ed8295ebc8fe8eb4d791df995cd4d9d664c
Esad Dursun <3723esad@gmail.com>
2022-01-29 21:46:45 +0100

    imaginary joystick implemented using in flame render function. if user drag, joystick appears

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -6,8 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:spacescape_clone/game/player.dart';
 
 class SpacescapeGame extends FlameGame with PanDetector {
-  Offset? _pointerStartPosition;
   late Player player;
+  Offset? _pointerStartPosition;
+  Offset? _pointerCurrentPosition;
+  final double _joystickRadius = 50;
+  final double _deadzoneRadius = 10;
 
   @override
   Future<void>? onLoad() async {
@@ -32,27 +35,60 @@ class SpacescapeGame extends FlameGame with PanDetector {
   }
 
   @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    if (_pointerStartPosition != null) {
+      canvas.drawCircle(_pointerStartPosition!, _joystickRadius,
+          Paint()..color = Colors.grey.withAlpha(50));
+    }
+
+    if (_pointerCurrentPosition != null) {
+      var delta = _pointerCurrentPosition! - _pointerStartPosition!;
+
+      if (delta.distance > _joystickRadius) {
+        delta = _pointerStartPosition! +
+            (Vector2(delta.dx, delta.dy).normalized() * _joystickRadius)
+                .toOffset();
+      } else {
+        delta = _pointerCurrentPosition!;
+      }
+
+      canvas.drawCircle(delta, _joystickRadius * 0.4,
+          Paint()..color = Colors.grey.withAlpha(100));
+    }
+  }
+
+  @override
   void onPanStart(DragStartInfo info) {
     _pointerStartPosition = info.raw.globalPosition;
+    _pointerCurrentPosition = info.raw.globalPosition;
   }
 
   @override
   void onPanUpdate(DragUpdateInfo info) {
-    final poinerCurrentPosition = info.raw.globalPosition;
+    _pointerCurrentPosition = info.raw.globalPosition;
 
-    var delta = poinerCurrentPosition - _pointerStartPosition!;
-    player.setMoveDirection(Vector2(delta.dx, delta.dy));
+    var delta = _pointerCurrentPosition! - _pointerStartPosition!;
+
+    if (delta.distance > _deadzoneRadius) {
+      player.setMoveDirection(Vector2(delta.dx, delta.dy));
+    } else {
+      player.setMoveDirection(Vector2.zero());
+    }
   }
 
   @override
   void onPanEnd(DragEndInfo info) {
     _pointerStartPosition = null;
+    _pointerCurrentPosition = null;
     player.setMoveDirection(Vector2.zero());
   }
 
   @override
   void onPanCancel() {
     _pointerStartPosition = null;
+    _pointerCurrentPosition = null;
     player.setMoveDirection(Vector2.zero());
   }
 }
