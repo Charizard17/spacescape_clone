ac83b8d598a7677aa1d9f3876121549f19584334
Esad Dursun <3723esad@gmail.com>
2022-01-29 21:28:07 +0100

    pan detector added. move player with dragging

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -2,9 +2,13 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
 import 'package:spacescape_clone/game/player.dart';
 
-class SpacescapeGame extends FlameGame {
+class SpacescapeGame extends FlameGame with PanDetector {
+  Offset? _pointerStartPosition;
+  late Player player;
+
   @override
   Future<void>? onLoad() async {
     await images.load('simpleSpace_tilesheet_2.png');
@@ -15,7 +19,7 @@ class SpacescapeGame extends FlameGame {
       rows: 6,
     );
 
-    Player player = Player(
+    player = Player(
       sprite: spriteSheet.getSpriteById(19),
       size: Vector2(80, 80),
       position: canvasSize / 2,
@@ -25,5 +29,30 @@ class SpacescapeGame extends FlameGame {
     add(player);
 
     return super.onLoad();
+  }
+
+  @override
+  void onPanStart(DragStartInfo info) {
+    _pointerStartPosition = info.raw.globalPosition;
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    final poinerCurrentPosition = info.raw.globalPosition;
+
+    var delta = poinerCurrentPosition - _pointerStartPosition!;
+    player.setMoveDirection(Vector2(delta.dx, delta.dy));
+  }
+
+  @override
+  void onPanEnd(DragEndInfo info) {
+    _pointerStartPosition = null;
+    player.setMoveDirection(Vector2.zero());
+  }
+
+  @override
+  void onPanCancel() {
+    _pointerStartPosition = null;
+    player.setMoveDirection(Vector2.zero());
   }
 }
