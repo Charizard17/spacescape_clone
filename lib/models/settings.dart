fb197a6758a6bb9ccc3e2aaeb05f87a3b3ff38fd
Esad Dursun <3723esad@gmail.com>
2022-02-06 23:13:19 +0100

    settings class created

---
+++ lib/models/settings.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+part 'settings.g.dart';
+
+@HiveType(typeId: 2)
+class Settings extends ChangeNotifier with HiveObjectMixin {
+  @HiveField(0)
+  bool _soundEffects = false;
+  bool get soundEffects => _soundEffects;
+  set soundEffects(bool value) {
+    _soundEffects = value;
+    notifyListeners();
+    save();
+  }
+
+  @HiveField(1)
+  bool _backgroundMusic = false;
+  bool get backgroundMusic => _backgroundMusic;
+  set backgroundMusic(bool value) {
+    _backgroundMusic = value;
+    notifyListeners();
+    save();
+  }
+
+  Settings({
+    bool soundEffects = false,
+    bool backgroundMusic = false,
+  })  : this._soundEffects = soundEffects,
+        this._backgroundMusic = backgroundMusic;
+}
